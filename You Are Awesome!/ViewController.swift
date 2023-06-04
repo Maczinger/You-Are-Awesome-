7a41b04b05cda9cde13470e0aab9730d3674e829
Juan Miguel <juan.escri@gmail.com>
2023-06-04 18:15:17 +0200

    Added functionality

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("👍🏽 View did load")
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("😎 The message button was pressed!")
+        let message = messageLabel.text
+        var printMessage = true
+        if let message
+        {
+            printMessage = message.isEmpty
+        }
+        
+        if printMessage
+        {
+            messageLabel.text = "You Are Awesome!"
+        }
+        else
+        {
+            messageLabel.text = ""
+        }
+    }
+    
 }
 
